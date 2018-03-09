7dae59f1c8ff1f24ea7323d66e55e8ae7e1e4e40
FEDERICO-MONTOL\federico.montoli <federico.montoli@pyxis.com.uy>
2018-03-09 19:41:09 -0300

    Config inicial en servlets IngresoWeb y Escolaridad

--- WEB-INF/src/sistema/web/servlets/EscolaridadServlet.java
+++ WEB-INF/src/sistema/web/servlets/EscolaridadServlet.java
@@ -1,7 +1,13 @@
 package sistema.web.servlets;
 
+import sistema.logica.ICapaLogica;
+import sistema.grafica.controladores.ContSingleton;
+import javax.servlet.http.HttpServlet;
 
-public class EscolaridadServlet  {
 
+public class EscolaridadServlet extends HttpServlet
+{
+	private ICapaLogica interfazFachada;
+	
 	
 }
--- WEB-INF/src/sistema/web/servlets/IngresoWebServlet.java
+++ WEB-INF/src/sistema/web/servlets/IngresoWebServlet.java
@@ -1,7 +1,11 @@
 package sistema.web.servlets;
 
+import sistema.logica.ICapaLogica;
+import sistema.grafica.controladores.ContSingleton;
+import javax.servlet.http.HttpServlet;
 
-public class IngresoWebServlet  {
-
+public class IngresoWebServlet extends HttpServlet
+{
+	private ICapaLogica interfazFachada=ContSingleton.getInstancia().getInterfazFachada();
 	
 }
